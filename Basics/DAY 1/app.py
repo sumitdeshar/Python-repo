8d402f1276c85fcad57034d21ddbe1e8af109691
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-09 23:58:02 +0545

    learning to print

---
+++ Basics/DAY 1/app.py
@@ -0,0 +1,21 @@
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+character_name = "John"
+character_age: int = 57
+is_male = True
+print("There was man named "+character_name+ ",")
+print("he was " + str(character_age) + " years and it is " + str(is_male) )
+character_name = "mike"
+character_age = 36
+print("There was another man named "+character_name+ ",")
+print("he was " + str(character_age) + " years")
+character_name = "Tom"
+character_age = 32
+print("There was man named "+character_name+ ",")
+print("he was " + str(character_age) + " years")
+character_name = "Micke"
+character_age = 36
+print("There was another man named "+character_name+ ",")
+print("he was " + str(character_age) + " years")

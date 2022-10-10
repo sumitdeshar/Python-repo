2710a38fff47f7e38262b4cfc0e66b3d62984a50
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-10 18:05:11 +0545

    basic calculator

---
+++ Basics/DAY 2/Calculator.py
@@ -0,0 +1,8 @@
+#num1 = int(input("Enter number"))
+#num2 = int(input("Enter number"))
+#result = num1 + num2
+num1 = input("Enter number")
+num2 = input("Enter number")
+result = float(num1) + float(num2)
+
+print(result)

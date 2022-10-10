9b71d4c3c07c8a702df5a4244120a23f569d21e3
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-10 18:10:24 +0545

    learning function

---
+++ Basics/DAY 3/functions.py
@@ -0,0 +1,8 @@
+
+def say_hi_and_hello(name,age):
+    print("hi  ! " + name)
+    print("you are " + str(age) + " years old\n")
+
+
+say_hi_and_hello("sumit", 21)
+say_hi_and_hello("saraswoti", 43)

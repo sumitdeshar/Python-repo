66be462b74513d2d6330b350beb2e300391dd895
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-12 21:28:34 +0545

    for loop

---
+++ Basics/DAY 4/for_loop.py
@@ -0,0 +1,17 @@
+friends = ["Amit","Prashil","Saman"]
+for letter in friends:
+    print(letter)
+for letter in "Oda Consultancy":#counts white space as well
+    print(letter)
+#for index in range(10): #can be used as index if it is used before but does quite work
+ #   print(letter)       #only print the last element of list of loop
+for index in range(10):
+    print(index)
+for index in range(3,10):
+    print(index)
+len(friends)
+for index in range(len(friends)):
+    print(index)
+for index in range(len(friends)):
+    print(friends[index])
+#for can loop through array string and others

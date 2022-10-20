20bd87ba8610f8f4583b81ae73a1ea8cd4c0e5c8
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-20 16:04:24 +0545

    new technique of nested loop updated

--- Basics/DAY 4/2D_list_and_nested_loop.py
+++ Basics/DAY 4/2D_list_and_nested_loop.py
@@ -9,3 +9,6 @@ print(number_grid[0][0])
 #for row in number_grid:
 #    for colomn in row:
 #        print(colomn)
+for row in number_grid:
+    for colomn in range(len(row)):
+        print(number_grid[colomn])

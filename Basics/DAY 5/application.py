0a64ed647b5128d046cf96f03c0f73f3b0a7e30e
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-28 14:20:19 +0545

    question class

--- Basics/DAY 5/application.py
+++ Basics/DAY 5/application.py
@@ -1,6 +1,6 @@
 from student import Student
     #file           class
 student1 = Student("Tuyu", "Nurse", 3.2, False)
-#student2 = Student("Pamu", True) incorrect alwyas should be completed
+#student2 = Student("Pamu", True) #incorrect class always should be completed
 
 print(student2.major)

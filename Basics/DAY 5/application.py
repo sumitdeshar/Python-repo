0d15d87596391cce7eebb1ffea41e73ead932488
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-28 13:52:24 +0545

    student class

---
+++ Basics/DAY 5/application.py
@@ -0,0 +1,6 @@
+from student import Student
+    #file           class
+student1 = Student("Tuyu", "Nurse", 3.2, False)
+#student2 = Student("Pamu", True) incorrect alwyas should be completed
+
+print(student2.major)

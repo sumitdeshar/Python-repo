95e071794190ecb0c1aa8026242ba6d11041dc47
Sumit Deshar <sumeetdsr@gmail.com>
2022-10-28 17:17:28 +0545

    class updated

--- Basics/DAY 5/application.py
+++ Basics/DAY 5/application.py
@@ -1,6 +1,7 @@
 from student import Student
     #file           class
-student1 = Student("Tuyu", "Nurse", 3.2, False)
-#student2 = Student("Pamu", True) #incorrect class always should be completed
+student1 = Student("Oscar", "Accounting", 3.4, False)
+student2 = Student("Phyllis", "Business", 3.8, True) #class parameter should always be completely filled
 
 print(student2.major)
+print(student2.on_honor_roll())
--- Basics/DAY 5/student.py
+++ Basics/DAY 5/student.py
@@ -5,3 +5,9 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
+    
+    def on_honor_roll(self,):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False

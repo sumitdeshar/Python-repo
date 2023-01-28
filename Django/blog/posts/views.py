d8b50a67c66640f8f31dc8f116d848e591884c65
Sumit Deshar <sumeetdsr@gmail.com>
2023-01-28 21:43:21 +0545

    django complete blog with basic feature

--- Django/blog/posts/views.py
+++ Django/blog/posts/views.py
@@ -3,4 +3,9 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    posts= Post.objects.all()
+    return render(request,'index.html',{'posts': posts})
+
+def post(request,pk):
+    posts= Post.objects.get(id=pk)
+    return render(request,'posts.html',{'posts': posts})

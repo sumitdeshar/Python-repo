8fcbc09f15969fbf4ca2e05410d1081ccf2c8503
Sumit Deshar <sumeetdsr@gmail.com>
2023-01-29 13:35:43 +0545

    initial commit

---
+++ Django/weatherdetector/weather/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST': 
+        city = request.POST ['city']
+    return render(request, 'index.html')

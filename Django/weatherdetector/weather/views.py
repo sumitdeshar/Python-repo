62a6bd1469eb851e0844847c68ac58d3b7af5fc2
Rayal kc <kcrayal450@gmail.com>
2023-01-31 22:23:58 +0545

    Testing To Check the name of city

--- Django/weatherdetector/weather/views.py
+++ Django/weatherdetector/weather/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     if request.method == 'POST': 
-        city = request.POST ['city']
-    return render(request, 'index.html')
+        city = request.POST['city']
+    return render(request, 'index.html',{'city':city})

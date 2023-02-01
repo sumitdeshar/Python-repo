fa42fbb52970493440582967fecb89cd4f8148e8
sumit deshar <sumeetdsr@gmail.com>
2023-02-01 19:26:10 +0545

    home complete sucessfully

--- Django/chat/chatapp/urls.py
+++ Django/chat/chatapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.home, name='home'),
+    path('<str:room>/',views.room, name='room'),
+    path('checkview',views.checkview, name='checkview'),
 ]
--- Django/chat/chatapp/views.py
+++ Django/chat/chatapp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from chatapp.models import Room, Message 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def room(request, room):
+    
+    return render(request, 'room.html')
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username) #redirecting the user to the existing room and passing the username with it
+    else:
+        new_room = Room.objects.create(name=room)   #creating the room to redirect the user if room doesnot exist
+        new_room.save()
+        return redirect('/'+room+'/?username='+username) 

3d4cdb3acd0968e168dab5938f098e1e55bb8164
sumit deshar <sumeetdsr@gmail.com>
2023-02-03 10:38:34 +0545

    django chat complete commit but there is a bug

--- Django/chat/chatapp/views.py
+++ Django/chat/chatapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
-from chatapp.models import Room, Message 
+from chatapp.models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def room(request, room):
-    
-    return render(request, 'room.html')
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name= room)
+    return render(request, 'room.html',{
+            'username': username,
+            'room': room,
+            'room_details': room_details  
+            })
 
 def checkview(request):
     room = request.POST['room_name']
@@ -18,3 +24,19 @@ def checkview(request):
         new_room = Room.objects.create(name=room)   #creating the room to redirect the user if room doesnot exist
         new_room.save()
         return redirect('/'+room+'/?username='+username) 
+    
+def send(request):
+    # input from ajax 
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+    
+    new_message = Message.objects.create(value= message, user= username, room= room_id)
+    new_message.save()
+    return HttpResponse('Message sent sucessfullly')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+    
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({'messages':list(messages.values())})
